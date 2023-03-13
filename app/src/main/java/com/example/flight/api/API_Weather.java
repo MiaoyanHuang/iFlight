ce4edc7d490a0d05a041599685fc73730491cffb
OliverHuang11 <113988464+OliverHuang11@users.noreply.github.com>
2023-03-13 22:41:29 +0800

    Update API_Weather.java

--- app/src/main/java/com/example/flight/api/API_Weather.java
+++ app/src/main/java/com/example/flight/api/API_Weather.java
@@ -6,15 +6,7 @@ import java.util.Objects;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-/**
- * Reference Function: Ok Http
- * Reference Method: Copy and Paste
- * API Name: Yahoo Weather
- * API Website: <a href="https://rapidapi.com/apishub/api/yahoo-weather5">...</a>
- * API Key: 1. H761157417@gmail.com: 225b54b282msh74ee543b8d95fc2p152f4fjsnbde570c0ea2c 每月26日刷新 100次查询
- *          2. I19017857@student.newinti.edu.my: 97d7fcce93msh2ceb3df9047f878p123262jsn8e7785cb6117  每月19号刷新 100次查询
- *          3. 1901856099@qq.com: f946e0f9b1msh260555160e012b3p172592jsn91956cd282e6 每月7号刷新 100次查询
- */
+
 public class API_Weather {
     private static final String TAG = "api_weather";
     private Response response = null;
