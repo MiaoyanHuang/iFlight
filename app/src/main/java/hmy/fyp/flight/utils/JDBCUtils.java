337194421140d8198f398287113a8c2c270439a7
Huang Miaoyan <miaoyanhuang0701@gmail.com>
2024-02-17 18:51:44 +0800

    update build.gradle

--- app/src/main/java/hmy/fyp/flight/utils/JDBCUtils.java
+++ app/src/main/java/hmy/fyp/flight/utils/JDBCUtils.java
@@ -8,7 +8,7 @@ import java.sql.DriverManager;
 public class JDBCUtils {
     private static final String TAG = "JDBCUtils";
     private static final String driver = "com.mysql.jdbc.Driver"; // MySql Driver
-    private static final String ip = "192.168.1.3:3306/"; //desktop ip
+    private static final String ip = "10.0.2.2:3306/"; // VM ip
     private static final String dbName = "flight"; // Database Name
     private static final String user = "root"; // Username
     private static final String password = "admin"; // Password
